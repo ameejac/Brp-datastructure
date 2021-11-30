9e9d8aada506a95c0e58341ca41b67451012b849
Ameejac <ameejacherukuru123@gmail.com>
2021-11-30 21:33:46 +0530

    ameeja[added:inserting the element at front and last]

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class Node
+    {
+
+        public int info;
+        public Node Link;
+        public Node(int data)
+        {
+            info = data;//constructor is defaultly calling and it will initialize the data
+            Link = null;
+        }
+    }
+    public class LinkedList
+    {
+        private Node Head;
+        public LinkedList()
+        {
+            Head = null;
+        }
+        
+        public void Insert(int data)
+        {
+            Node temp = new Node(data);
+           this.Head = temp;
+            Console.WriteLine("insert the data" + this.Head.info);
+        }
+        public void AddLast(int data)
+        {
+            Node new_node = new Node(data);
+            if(this.Head == null)
+            {
+                this.Head = new_node; 
+            }
+            else
+            {
+                Node temp = this.Head;
+                while (temp.Link != null)
+                {
+                    temp = temp.Link;  
+                }
+                temp.Link = new_node;
+                Console.WriteLine("adding the elements at  last:" + new_node.info);
+            }
+        }
+       
+        public void AddFront(int data)
+        {
+            Node new_node = new Node(data);
+            new_node.Link = this.Head;
+            this.Head = new_node;
+            Console.WriteLine("add elements at front:" + new_node.info);
+        }
+        public void Display()
+        {
+            Node New_node = this.Head;
+            if (New_node == null)
+            {
+                Console.WriteLine("if the node is empty");
+            }
+            else
+            {
+                while(New_node != null)
+                {
+                    Console.WriteLine("if the node is not empty:" + New_node.info);
+                    New_node = New_node.Link;
+
+                }
+            }
+        }
+    }
+    
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList obj = new LinkedList();
+            obj.Insert(20);
+            obj.AddLast(30);
+            obj.AddFront(10);
+            obj.Display();
+
+        }
+    }
+}
